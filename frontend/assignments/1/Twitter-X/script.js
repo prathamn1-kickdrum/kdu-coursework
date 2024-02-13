79c3aa76ed5c3e9394a0bad77f2374534aca8784
prathamn1-kickdrum <pratham.nagaria@kickdrumtech.com>
2024-02-13 12:48:18 +0530

    Added Sonar tests

--- frontend/assignments/1/Twitter-X/script.js
+++ frontend/assignments/1/Twitter-X/script.js
@@ -16,36 +16,56 @@ const LIKE_BUTTON_SVG = `
 const LIKE_SVG = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><g><path fill="#ff657e" d="M20.884 13.19c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path></g></svg>
 `;
-const tweetInput = document.querySelector("#tweet-input");
-const postButton = document.querySelector("#tweet-btn");
-const postWrapper = document.querySelector("#post-wrapper");
-const tweetFob = document.querySelector("#tweet-fob")
-const profileIcon = document.getElementsByClassName("profile-icon")
-const tweetBox = document.getElementsByClassName("msg")
-const navSection = document.getElementsByClassName("navigation-section")
-const mobileNav = document.getElementsByClassName('mobile-nav')
-const mobileNavActions = document.getElementsByClassName('mobile-nav__actions')
+
+const myImage = document.getElementsByClassName('prof-image');
+const inputTweetElement = document.querySelector("#tweet-input");
+const container = document.querySelector("#post-container");
+const tweetContainer = document.getElementsByClassName("tweet-box")
+
+const navBar = document.getElementsByClassName('mobile-nav')
+const navOps = document.getElementsByClassName('mobile-nav-ops')
 const likeCount = document.getElementsByClassName('likes-count')[0]
 likeCount.textContent = 0;
 
 
-postButton.addEventListener('click', () => {
-    const tweetValue = tweetInput.value;
-    console.log(tweetValue)
+const navContainer = document.getElementsByClassName("navigation-section")
+window.addEventListener('resize',()=> {
+    if(this.window.innerWidth>570) {
+        navContainer[0].style.display='flex';
+    }else {
+        navContainer[0].style.display='none';
+
+    }
+})
+
+
+const postBtn = document.querySelector("#tweet-btn");
+
+postBtn.addEventListener('click', () => {
+    const tweetValue = inputTweetElement.value;
     createPostElement(tweetValue);
 })
 
-profileIcon[0].addEventListener('click', () => {
-    navSection[0].classList.add('display-on-flex')
+const iconProfile = document.getElementsByClassName("profile-icon")
+
+
+iconProfile[0].addEventListener('click',()=> {
+    navContainer[0].style.display='flex';
 })
+
+myImage[0].addEventListener('click',()=> {
+    navContainer[0].style.display='none';
+})
+
+const tweetFob = document.querySelector("#tweet-fob")
 
 tweetFob.addEventListener('click', () => {
-    tweetBox[0].classList.add('display-on-flex')
-    mobileNav[0].classList.add('display-none')
-    mobileNavActions[0].classList.add('display-on-flex')
+    tweetContainer[0].classList.add('display-on-flex')
+    navBar[0].classList.add('display-none')
+    navOps[0].classList.add('display-on-flex')
 })
 
-function extractHashtags(inputString) {
+const getTweetTags = (inputString) => {
     const hashtagRegex = /#(\S+)/g;
 
     const hashtags = [];
@@ -57,28 +77,28 @@ function extractHashtags(inputString) {
     return [normalText.trim(), ...hashtags];
 }
 
-function createPostElement(tweetValue) {
+const createPostElement = (tweetValue) => {
     let liked = false;
 
     if (tweetValue.length === 0) {
         return;
     }
 
-    const inputArray = extractHashtags(tweetValue);
+    const inputArray = getTweetTags(tweetValue);
 
     const postContainer = document.createElement('div');
     postContainer.classList.add('posts');
 
-    const profileImg = document.createElement('img');
-    profileImg.classList.add('posts__profile');
-    profileImg.src = '/profile-pic.ad247f4d.png';
-    profileImg.alt = '';
+    const profileElement = document.createElement('Element');
+    profileElement.classList.add('posts-profile');
+    profileElement.src = './assets/profile-pic.png';
+    profileElement.alt = 'profile-pic';
 
     const postInfo = document.createElement('div');
-    postInfo.classList.add('posts__info');
+    postInfo.classList.add('posts-info');
 
     const postTitle = document.createElement('div');
-    postTitle.classList.add('posts__title');
+    postTitle.classList.add('posts-title');
 
     const titleName = document.createElement('h1');
     titleName.textContent = 'Nitesh Gupta';
@@ -89,53 +109,53 @@ function createPostElement(tweetValue) {
     const titleTime = document.createElement('span');
     titleTime.textContent = '1s';
 
-    const moreInfoImg = document.createElement('img');
-    moreInfoImg.src = '/three_dots.ca3064e4.svg';
-    moreInfoImg.alt = 'More Info';
+    const moreInfoElement = document.createElement('Element');
+    moreInfoElement.src = './assets/three_dots.svg';
+    moreInfoElement.alt = 'More Info';
 
     const postContent = document.createElement('div');
-    postContent.classList.add('posts__content');
+    postContent.classList.add('posts-content');
 
     const contentText = document.createElement('p');
     contentText.textContent = inputArray[0];
 
     const postActions = document.createElement('div');
-    postActions.classList.add('posts__actions');
+    postActions.classList.add('post-ops');
 
-    const commentImg = document.createElement('img');
-    commentImg.src = '/comment.145a5825.svg';
-    commentImg.alt = 'Comment';
+    const commentElement = document.createElement('img');
+    commentElement.src = './assets/comment.svg';
+    commentElement.alt = 'Comment';
 
-    const retweetImg = document.createElement('img');
-    retweetImg.src = '/retweet.cc5912f0.svg';
-    retweetImg.alt = 'Retweet';
+    const retweetElement = document.createElement('img');
+    retweetElement.src = './assets/retweet.svg';
+    retweetElement.alt = 'Retweet';
 
     const likeDiv = document.createElement('div');
     const likeCountSpan = document.createElement('span');
     likeCountSpan.classList.add('likes-count');
     likeCountSpan.textContent = 0;
-    likeDiv.classList.add('like-button__wrapper');
+    likeDiv.classList.add('like-button-container');
     likeDiv.innerHTML = LIKE_BUTTON_SVG;
 
-    const statsImg = document.createElement('img');
-    statsImg.src = '/stats.e0073341.svg';
-    statsImg.alt = 'Stats';
+    const statsElement = document.createElement('img');
+    statsElement.src = './assets/stats.svg';
+    statsElement.alt = 'Stats';
 
     const subActions = document.createElement('div');
-    subActions.classList.add('posts__subactions');
+    subActions.classList.add('posts-subactions');
 
-    const bookmarkImg = document.createElement('img');
-    bookmarkImg.src = '/bookmark.14f36264.svg';
-    bookmarkImg.alt = 'Bookmark';
+    const bookmarkElement = document.createElement('img');
+    bookmarkElement.src = './assets/bookmark.svg';
+    bookmarkElement.alt = 'Bookmark';
 
-    const shareImg = document.createElement('img');
-    shareImg.src = '/share.0c27b6b5.svg';
-    shareImg.alt = 'Share';
+    const shareElement = document.createElement('img');
+    shareElement.src = './assets/share.svg';
+    shareElement.alt = 'Share';
 
     postTitle.appendChild(titleName);
     postTitle.appendChild(titleHandle);
     postTitle.appendChild(titleTime);
-    postTitle.appendChild(moreInfoImg);
+    postTitle.appendChild(moreInfoElement);
 
     postContent.appendChild(contentText);
 
@@ -145,8 +165,8 @@ function createPostElement(tweetValue) {
         postContent.appendChild(contentTags);
     }
 
-    postActions.appendChild(commentImg);
-    postActions.appendChild(retweetImg);
+    postActions.appendChild(commentElement);
+    postActions.appendChild(retweetElement);
 
     likeDiv.appendChild(likeCountSpan);
     postActions.appendChild(likeDiv);
@@ -154,22 +174,20 @@ function createPostElement(tweetValue) {
     likeDiv.addEventListener('click', () => {
         liked = !liked;
         if (!liked) {
-            console.log(likeCountSpan)
             likeCountSpan.textContent = likeCountSpan.textContent * 1 - 1;
             likeDiv.innerHTML = LIKE_BUTTON_SVG;
             likeDiv.appendChild(likeCountSpan)
         } else {
-            console.log(likeCountSpan)
             likeCountSpan.textContent = likeCountSpan.textContent * 1 + 1;
             likeDiv.innerHTML = LIKE_SVG;
             likeDiv.appendChild(likeCountSpan)
         }
     });
 
-    postActions.appendChild(statsImg);
+    postActions.appendChild(statsElement);
 
-    subActions.appendChild(bookmarkImg);
-    subActions.appendChild(shareImg);
+    subActions.appendChild(bookmarkElement);
+    subActions.appendChild(shareElement);
 
     postActions.appendChild(subActions);
 
@@ -177,31 +195,29 @@ function createPostElement(tweetValue) {
     postInfo.appendChild(postContent);
     postInfo.appendChild(postActions);
 
-    postContainer.appendChild(profileImg);
+    postContainer.appendChild(profileElement);
     postContainer.appendChild(postInfo);
 
-    postWrapper.appendChild(postContainer);
-    postButton.style.backgroundColor = 'rgba(29, 156, 240, 0.429)';
-    tweetInput.value = "";
-    postButton.disabled = true;
+    container.appendChild(postContainer);
+    postBtn.style.backgroundColor = 'rgba(29, 156, 240, 0.429)';
+    inputTweetElement.value = "";
+    postBtn.disabled = true;
 }
 
-tweetInput.addEventListener('keyup', function () {
-    let inputValue = tweetInput.value.trim();
-    postButton.disabled = inputValue.length > 0 ? false : true;
+inputTweetElement.addEventListener('keyup', function () {
+    let inputValue = inputTweetElement.value.trim();
+    postBtn.disabled = inputValue.length > 0 ? false : true;
 
-    postButton.style.backgroundColor = !inputValue ? 'rgba(29, 156, 240, 0.429)' : 'rgb(29, 155, 240)';
+    postBtn.style.backgroundColor = !inputValue ? 'rgba(29, 156, 240, 0.429)' : 'rgb(29, 155, 240)';
 });
 
 const likeButton = document.getElementsByClassName('like-button');
-console.log(likeButton)
 let liked = false;
 
 for (const element of likeButton) {
     element.addEventListener('click', () => {
         liked = !liked;
         if (!liked) {
-            console.log(likeCount.textContent);
             likeCount.textContent = likeCount.textContent * 1 - 1;
             element.innerHTML = LIKE_BUTTON_SVG;
         } else {
